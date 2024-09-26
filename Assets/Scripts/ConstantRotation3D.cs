00a511e7a9abcb06c5af5dd901d3d453f5fad89c
AdrianAB30 <adrian_12314@outlook.com>
2024-09-26 16:22:09 -0500

    Implemented Plane Move and Earth

---
+++ Assets/Scripts/ConstantRotation3D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Mathematics.Week6
+{
+    public class ConstantRotation3D : MonoBehaviour
+    {
+        public float WorldRotation = 20f;
+
+        void Update()
+        {
+            this.transform.Rotate(new Vector3(WorldRotation, 0, 0) * Time.deltaTime);
+        }
+    }  
+}
---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Mathematics.Week6
+{
+    public class PlaneController : MonoBehaviour
+    {
+        private Rigidbody myRBD;
+        [SerializeField] private float velocityModifier = 5f;
+        [SerializeField] private float rotationAngle = 35f;
+        [SerializeField] private float rotationSmooth = 5f;
+        [SerializeField] private int life = 3;
+        //public AudioSource audioDamage;
+        //public AudioSource audioLose;
+        //public TextMeshProUGUI textLife;
+
+        private Vector2 inputMovement;
+        private Quaternion targetRotation;
+
+        private void Awake()
+        {
+            myRBD = GetComponent<Rigidbody>();
+        }
+        void Update()
+        {
+            UpdateRotation();
+        }
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            inputMovement = context.ReadValue<Vector2>();
+
+            myRBD.velocity = new Vector3(inputMovement.x * velocityModifier, inputMovement.y * velocityModifier, 0f);
+
+            if (inputMovement.x > 0) 
+            {
+                targetRotation = Quaternion.Euler(0f, 0f, -rotationAngle);
+            }
+            else if (inputMovement.x < 0) 
+            {
+                targetRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+            }
+            else if (inputMovement.y > 0)
+            {
+                targetRotation = Quaternion.Euler(-rotationAngle, 0f, 0f);
+            }
+            else if (inputMovement.y < 0) 
+            {
+                targetRotation = Quaternion.Euler(rotationAngle, 0f, 0f);
+            }
+            else
+            {
+                targetRotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+        private void UpdateRotation()
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSmooth * Time.deltaTime);
+        }
+    }
+}
